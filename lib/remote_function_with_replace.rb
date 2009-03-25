fed21f9b3f444c2dd14e23f2e1d4e1156dd3125b
Will Bryant <will.bryant@gmail.com>
2009-03-26 00:57:49 +1300

    added :eval_or_update and :eval_or_replace which work around Prototype's silly Ajax.Updater handling of text/javascript responses - it both executes the code and inserts it in the page, which is not useful behavior

--- lib/remote_function_with_replace.rb
+++ lib/remote_function_with_replace.rb
@@ -2,13 +2,19 @@ module ActionView
   module Helpers
     module PrototypeHelper
       def remote_function_with_replace(options)
-  	    if replace = options.delete(:replace)
-  	      replace = {:complete => replace} unless replace.is_a?(Hash)
-	        [:complete, :success, :failure].each { |callback| options[callback] = "$('#{replace[callback]}').replace(request.responseText);#{options[callback]}" if replace[callback] }
-    		  remote_function_without_replace(options)
-    		else
-    		  remote_function_without_replace(options)
+        prototype_javascript_content_type_regexp = '/^\s*(text|application)\/(x-)?(java|ecma)script(;.*)?\s*$/i' # from prototype.js Ajax.Request#respondToReadyState
+        
+  	    if target = options.delete(:replace)
+  	      target = {:complete => target} unless target.is_a?(Hash)
+	        [:complete, :success, :failure].each { |callback| options[callback] = "$('#{target[callback]}').replace(request.responseText);#{options[callback]}" if target[callback] }
+  	    elsif target = options.delete(:eval_or_replace)
+  	      target = {:complete => target} unless target.is_a?(Hash)
+	        [:complete, :success, :failure].each { |callback| options[callback] = "if (!(request.getHeader('Content-type') || '').match(#{prototype_javascript_content_type_regexp})) {$('#{target[callback]}').replace(request.responseText);} #{options[callback]}" if target[callback] } # due to poor choice of parameter names, request.getHeader actually returns response headers
+  	    elsif target = options.delete(:eval_or_update)
+  	      target = {:complete => target} unless target.is_a?(Hash)
+	        [:complete, :success, :failure].each { |callback| options[callback] = "if (!(request.getHeader('Content-type') || '').match(#{prototype_javascript_content_type_regexp})) {$('#{target[callback]}').update(request.responseText);} #{options[callback]}" if target[callback] } # ditto
     		end
+  		  remote_function_without_replace(options)
   		end
   		
       alias_method_chain :remote_function, :replace
